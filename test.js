17ffefddffe848c5ac400d60329b1501917a4521
Neil Manvar <neil@sentry.io>
2020-08-05 21:12:28 -0700

    got it working for minified using "dsn:"

--- test.js
+++ test.js
@@ -1,23 +1,55 @@
 document.getElementById("test").addEventListener('click', () => {
     console.log("Popup DOM fully loaded and parsed");
+        // alert('abc')
 
-    function modifyDOM() {
-        //You can play with your DOM here or check URL against your regex
+    function getScripts() {
         console.log('Tab script:');
-        debugger;
         console.log(document.body);
-        return document.body.innerHTML;
+        return Array.from(document.getElementsByTagName("script")).map(h => h.outerHTML);
     }
 
-    //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
-    chrome.tabs.executeScript({
-        code: '(' + modifyDOM + ')();' //argument here is a string but function.toString() returns function's code
-    }, (results) => {
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, (tabs) => {
+            let url = tabs[0].url;
+            let hasSentry = false;
 
-        // TODO: sasha get results here...get this to hit
-        debugger;
-        //Here we have just the innerHTML and not DOM structure
-        console.log('Popup script:')
-        console.log(results[0]);
-    });
+            chrome.tabs.executeScript({
+                code: '(' + getScripts + ')();' //argument here is a string but function.toString() returns function's code
+            }, (results) => {
+                // alert(JSON.stringify(results));
+                if (results.length > 0 && results[0].length > 0) {
+                    for (const scriptString of results[0]) {
+                        if (scriptString.includes('src=')) {
+                            let srcRegEx = /src="(.*?)"/g,
+                                source = srcRegEx.exec(scriptString),
+                                scriptSrc = source[1];
+
+                            if (!scriptSrc.includes('http')) {
+                                scriptSrc = url.slice(0, -1) + scriptSrc;
+                            }
+                            fetch(scriptSrc).then(response => response.text()).then(text => {
+                                if (text.includes('dsn:')) { // to figure out if using sentry.io or on-prem/rev-proxy
+                                    alert('yeahh buddy we got sentry in hereee');
+                                    hasSentry = true;
+                                }
+                            });
+                        } else if (scriptString.toLowerCase().includes('sentry')) {
+                            alert('we also got sentry in the house');
+                            // var hasSentry = true;
+                        }
+
+                    }
+                } else {
+                    console.log("Did not find any script tags");
+                }
+            });
+        }
+
+    );
+
 });
+
+
+// TODO solve for etsy: where they have it on the window.Sentry (i think its cause they are using loader)
