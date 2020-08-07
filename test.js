faaf61a15b7779ed15b0a67cd9473cc2b95e00b5
Neil Manvar <neil@sentry.io>
2020-08-06 18:07:12 -0700

    Alerting as necesary and using while loop

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 document.getElementById("test").addEventListener('click', () => {
-
     function getScripts() {
         return Array.from(document.getElementsByTagName("script")).map(h => h.outerHTML);
     }
@@ -16,7 +15,6 @@ document.getElementById("test").addEventListener('click', () => {
         if (!!results[0] && results[0] === "true") {
             hasSentry = true;
         } else {
-
             chrome.tabs.query({
                     active: true,
                     currentWindow: true
@@ -26,9 +24,10 @@ document.getElementById("test").addEventListener('click', () => {
                     chrome.tabs.executeScript({
                         code: '(' + getScripts + ')();' //argument here is a string but function.toString() returns function's code
                     }, (results) => {
-                        // alert(JSON.stringify(results));
                         if (results.length > 0 && results[0].length > 0) {
-                            for (const scriptString of results[0]) {
+                            let i = 0;
+                            while (i < results[0].length && !hasSentry) {
+                                const scriptString = results[0][i];
                                 if (scriptString.includes('src=') && !scriptString.includes('src="chrome-extension://')) {
                                     let srcRegEx = /src="(.*?)"/g,
                                         source = srcRegEx.exec(scriptString),
@@ -39,33 +38,32 @@ document.getElementById("test").addEventListener('click', () => {
                                     }
                                     fetch(scriptSrc).then(response => response.text()).then(text => {
                                         if (text.includes('dsn:')) { // to figure out if using sentry.io or on-prem/rev-proxy, as well as Raven
-                                            alert('yeahh buddy we got sentry in hereee');
                                             hasSentry = true;
+                                            alert('has Sentry via NPM/YARN');
                                         }
+                                    }).catch( error =>  {
+                                        alert(error);
+                                        console.log(error);
                                     });
                                 } else if (scriptString.toLowerCase().includes('dsn:')) {
-                                    alert('we also got sentry in the house');
                                     hasSentry = true;
+                                    alert('has Sentry via NPM/YARN');
                                 }
 
+                                i++;
                             }
+
                         } else {
                             console.log("Did not find any script tags");
                         }
                     });
                 }
-
             );
-
         }
-
+        if (hasSentry) {
+            alert('has Sentry via CDN');
+        }
     });
-
-
-
-
-
 });
-
 
 // TODO solve for etsy: where they have it on the window.Sentry (i think its cause they are using loader)
